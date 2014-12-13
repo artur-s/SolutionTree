307d1accf1b98814c0076048ded8d20b3844ae63
artur-s <arturs@iqmetrix.com>
2014-12-13 12:45:30 -0600

    Bump version to 0.1.0

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,8 @@
+#### 0.1.0 - December 13 2014
+* Add counting items and computing depth of projects hierarchy
+* Simpified parsing of solution folder items
+* Add reference to FSharp.Core NuGet package
+
 
 #### 0.0.1 - November 17 2014
 * Initial release
--- src/SolutionTree/AssemblyInfo.fs
+++ src/SolutionTree/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("SolutionTree")>]
 [<assembly: AssemblyProductAttribute("SolutionTree")>]
 [<assembly: AssemblyDescriptionAttribute("Parses Visual Studio solution file `.sln` and returns its projects structure.")>]
-[<assembly: AssemblyVersionAttribute("0.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.1"
+    let [<Literal>] Version = "0.1.0"
